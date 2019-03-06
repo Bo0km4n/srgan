63a936d45a62b66aeabf2ffaa85fd3abd1a29b55
川部 勝也 <s06495@10015413pc.cyberagent.co.jp>
2019-03-06 22:13:58 +0900

    update

--- x2_train.py
+++ x2_train.py
@@ -136,6 +136,8 @@ def train_x2(config):
     print('sample HR sub-image:', sample_imgs_384.shape, sample_imgs_384.min(), sample_imgs_384.max())
     sample_imgs_192 = tl.prepro.threading_data(sample_imgs_384, fn=downsample_fn_x2)
     print('sample LR sub-image:', sample_imgs_192.shape, sample_imgs_192.min(), sample_imgs_192.max())
+    from IPython.core.debugger import Tracer; Tracer()() 
+    
     tl.vis.save_images(sample_imgs_192, [ni, ni], save_dir_ginit + '/_train_sample_192.png')
     tl.vis.save_images(sample_imgs_384, [ni, ni], save_dir_ginit + '/_train_sample_384.png')
     tl.vis.save_images(sample_imgs_192, [ni, ni], save_dir_gan + '/_train_sample_192.png')
@@ -155,7 +157,6 @@ def train_x2(config):
         for idx in range(0, len(train_hr_img_list), batch_size):
             step_time = time.time()
             b_imgs_list = train_hr_img_list[idx : idx + batch_size]
-            # from IPython.core.debugger import Tracer; Tracer()() 
             b_imgs = tl.prepro.threading_data(b_imgs_list, fn=get_imgs_fn, path=config.TRAIN.hr_img_path)
             b_imgs_384 = tl.prepro.threading_data(b_imgs, fn=crop_sub_imgs_fn, is_random=True)
             b_imgs_192 = tl.prepro.threading_data(b_imgs_384, fn=downsample_fn_x2)
