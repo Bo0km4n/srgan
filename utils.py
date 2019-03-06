36d9ca0abd22e9765008aa66de39c53787f04be7
川部 勝也 <s06495@10015413pc.cyberagent.co.jp>
2019-03-06 20:24:46 +0900

    update

--- utils.py
+++ utils.py
@@ -20,6 +20,13 @@ def crop_sub_imgs_fn(x, is_random=True):
     # x = (x - 0.5)*2
     return x
 
+def crop_sub_imgs_fn_x2(x, is_random=True):
+    x = crop(x, wrg=192, hrg=192, is_random=is_random)
+    x = x / (255. / 2.)
+    x = x - 1.
+    # x = (x - 0.5)*2
+    return x
+
 def downsample_fn(x):
     # We obtained the LR images by downsampling the HR images using bicubic kernel with downsampling factor r = 4.
     x = imresize(x, size=[96, 96], interp='bicubic', mode=None)
